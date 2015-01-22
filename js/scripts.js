b79299127c1176256366792e71d4d4c7b5f01c0b
Patrick Conant <patrick@prcapps.com>
2015-01-22 17:19:06 -0500

    Added search

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,50 @@
+donor_list = 
+
+  [
+  'Chad Jones',
+  'Joe Smith',
+  'Lisa Michelson'
+  ];
+
+
+  for(index in donor_list){
+    donor = donor_list[index];
+    donor_key = donor.replace(/\s+/g, '');
+
+    $(".donor-list").append(
+      "<li class='donor-"+donor_key+"'>"+donor+"</li>"
+      );
+  }
+
+
 $(document).ready(function(){
+
+  $(document).on('click', '.input', function(){
+    value = $(this).val();
+
+    if(! value){
+      $('.donor-list li').fadeIn('fast');
+      return;
+    }
+
+    console.log(value);
+    $('.donor-list li').each(function(){
+      test_val = $(this).html();
+      last_name = test_val.split(' ')[1];
+      console.log(last_name);
+
+      console.log(test_val);
+      if(last_name.indexOf(value) == -1){
+        $(this).fadeOut('fast');
+      }
+      else{
+        $(this).fadeIn('fast');
+      }
+
+    });    
+
+   });
+
 
   // START MAIN SLIDER
   slidr.create('slidr-level-1', {
@@ -26,6 +72,9 @@ $(document).ready(function(){
      var $firstInput = $(':input').first().focus();
      jsKeyboard.currentElement = $firstInput;
      jsKeyboard.currentElementCursorPosition = 0;
+
+
+     
   });  
 
 });
